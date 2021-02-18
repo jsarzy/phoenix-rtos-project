8c321c5da0912b49abe5e177d53a5cb51e857639
Jakub Sarzyński <jakub.sarzynski@phoenix-rtos.com>
2021-02-18 15:58:23 +0100

    kernel tests: add Unity and basic tests

--- docker-build.sh
+++ docker-build.sh
@@ -12,9 +12,9 @@ fi
 
 if [ "$#" -eq 1 ] && [ "$1" = "bash" ]; then
     # run interactive shell - using ROOT user
-    exec docker run -it  --rm -v "$(pwd):/src" -w /src -e TARGET --entrypoint bash $DOCKER_IMG_NAME
+    exec docker run -it  --rm -v "$(pwd):/src" -w /src -e TARGET -e KERNEL_TEST_GROUPS --entrypoint bash $DOCKER_IMG_NAME
 else
     # run build - use our own UID/GID to create files with correct owner
-    exec docker run -it --user "$DOCKER_USER" --rm -v "$(pwd):/src:delegated" -w /src "${TMPFS_OVERLAY[@]}" -e TARGET $DOCKER_IMG_NAME "$@"
+    exec docker run -it --user "$DOCKER_USER" --rm -v "$(pwd):/src:delegated" -w /src "${TMPFS_OVERLAY[@]}" -e TARGET -e KERNEL_TEST_GROUPS $DOCKER_IMG_NAME "$@"
 fi
 
